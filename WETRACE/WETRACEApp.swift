647f11c1dcbc9c4e71a512e149f31a55620244e5
Momo L'As <momolas@users.noreply.github.com>
2026-01-04 15:13:40 +0100

    Initial Commit

---
+++ WETRACE/WETRACEApp.swift
@@ -0,0 +1,17 @@
+//
+//  WETRACEApp.swift
+//  WETRACE
+//
+//  Created by Mo on 04/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct WETRACEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
